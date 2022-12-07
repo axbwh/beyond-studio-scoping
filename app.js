62fcd3dd7fe298c4d3d61b6e120b1d98829a78b2
axbwh <bannwarth.alexandre@gmail.com>
2022-12-07 18:49:54 +1300

    fallback puck cropping

--- app.js
+++ app.js
@@ -860,6 +860,7 @@ const onReady = async () => {
 
 
     let GPUTier = await getGPUTier()
+    console.log('heyooo')
 
     if(GPUTier.tier > 0){
     // create curtains instance
@@ -875,9 +876,40 @@ const onReady = async () => {
 const fallback = ()=> {
 
     document.querySelectorAll('.casestudy-img-wrapper').forEach( (e, i) =>{
-        e.style.clipPath = i % 2 === 0 ? 'path("M356,4.4c341.2,38.1,513.2,204.3,523.7,282.3c27.4,123.6-242.9,288.3-328.9,338c-86.1,49.6-298.4,151.4-439.6,89.5C-1.8,664.6-6.8,459.1,4.8,362.5C32,229.5,27.2-32.3,356,4.4z")'
-        : 'path("M877.4,362.5C889,459.1,884,664.6,771,714.2c-141.2,61.9-353.5-39.9-439.6-89.5c-86.1-49.6-356.3-214.4-328.9-338C13,208.7,184.9,42.5,526.2,4.4C855-32.3,850.1,229.5,877.4,362.5z")' 
+        let evenPath = "M0.404,0.006 c0.387,0.052,0.582,0.279,0.594,0.385 c0.031,0.169,-0.276,0.393,-0.373,0.461 c-0.098,0.068,-0.339,0.207,-0.499,0.122 C-0.002,0.907,-0.008,0.627,0.005,0.495 C0.036,0.313,0.031,-0.044,0.404,0.006"
+        let oddPath ="M0.996,0.495 C1,0.627,1,0.907,0.875,0.975 c-0.16,0.084,-0.401,-0.054,-0.499,-0.122 c-0.098,-0.068,-0.404,-0.293,-0.373,-0.461 C0.015,0.285,0.21,0.058,0.597,0.006 C0.97,-0.044,0.965,0.313,0.996,0.495" 
+        let svgpath = 'http://www.w3.org/2000/svg'
+        if(i < 2){
+        
+
+            let svg = document.createElementNS(svgpath, 'svg')
+            svg.setAttributeNS(null, 'viewBox', "0 0 882 753")
+            svg.style.position = 'absolute'
+            svg.style.width = '100%'
+            svg.setAttribute( 'preserveAspectRatio', "xMidYMid meet")
+            svg.setAttribute('xmlns', 'http://www.w3.org/2000/svg')
+    
+            let clipPath = document.createElementNS(svgpath, 'clipPath')
+            clipPath.setAttributeNS(null, 'id', `clipper${i}`)
+            clipPath.setAttributeNS(null, 'clipPathUnits', `objectBoundingBox`)
+            let path = document.createElementNS(svgpath, 'path')
+            path.setAttributeNS(null, 'd', i % 2 == 0 ? evenPath : oddPath)
+            clipPath.appendChild(path)
+            svg.appendChild(clipPath)
+            e.appendChild(svg);
+            }
+        
+            let div = document.createElement('div')
+            div.style.position ='absolute'
+            div.style.height = '100%'
+            div.style.maxWidth ='100%'
+            div.style.aspectRatio = 1.17
+            div.style.clipPath = `url(#clipper${i % 2 == 0 ? 0 : 1})`
+            div.appendChild(e.querySelector('img'))
+            e.appendChild(div)
     })
+
+
     anime({
         targets: '#preloader',
         opacity: 0,
