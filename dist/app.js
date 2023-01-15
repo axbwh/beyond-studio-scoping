bc2fe8819679da43673060680f4548de080eef4f
axbwh <bannwarth.alexandre@gmail.com>
2023-01-16 10:31:28 +1300

    3 scrollmult

--- dist/app.js
+++ dist/app.js
@@ -1024,7 +1024,7 @@ class App {
         document.addEventListener("mousemove", _mouse.bind(this), false);
         this.scroller = new (0, _virtualScrollDefault.default)({
             preventTouch: false,
-            touchMultiplier: 4
+            touchMultiplier: 3
         });
         this.scroller.on((event)=>{
             this.onScroll(event);
